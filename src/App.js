b4dff534406ebca0625c82975e4c10e24f226de6
Patrick <74840936+patllamas@users.noreply.github.com>
2021-09-07 18:33:06 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='wallpaper' className="wallpaper" data-image='.\images\hitoshi-namura-MyiYvI-EKis-unsplash.jpg'></div>
+      <div className="content">
+        <aside className="side">
+          <figure id='picture' className="picture">
+            <div className="picture-shadow"></div>
+            <img
+              id="pictureImage"
+              className="picture-image"
+              src={require("./images/pat.jpg").default}
+              alt="Patrick Llamas"
+              width="320"
+              height="320"
+            />
+          </figure>
+        </aside>
+        <main className="about">
+          <h1 className="name">Hey, I'm Patrick Llamas</h1>
+          <p className="job">Web Developer</p>
+          <hr className="hr" />
+          <div className="description">
+            <p>I build websites and possess a passion for music and games</p>
+          </div>
+          <div className="contact">
+            <a className="button" href="mailto:pat.llamas@outlook.com">
+              Let's talk
+            </a>
+          </div>
+          <ul className="social">
+            <li>
+              <a href='https://github.com/patllamas/'>
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/patllamas/'>
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </main>
+      </div>
     </div>
   );
 }
